b385c0c6d1df781e91ea8c1a21281b9173f1ad40
Lisa <lis.uhlen@gmail.com>
2020-09-15 20:41:20 +0200

    My first commit of the Pro Found ma1 file, script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// question 1
+var firstVar;
+
+// question 2
+
+var name = "Lisa";
+
+// question 3
+
+var number = 10;
+
+// question 4
+
+var division = 20 / 5;
+
+//question 5
+
+typeof "frog";
+
+//question 6
+
+var orderHasShipped = true;
+
+// question 7
+
+if (orderHasShipped === true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// question 8
+
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
